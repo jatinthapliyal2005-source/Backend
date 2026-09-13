c1fe2b37c692d40afff4b8c5200ecb7c82b225dc
Jatin Thapliyal <jatinthapliyal2005@gmail.com>
2026-09-14 03:19:49 +0530

    update

--- day-1-Node-js/server.js
+++ day-1-Node-js/server.js
@@ -1,1 +1,10 @@
-console.log("hellow")
+let http=require("http");
+let server=http.createServer((req,res)=>{
+    console.log("hellow i m server")
+    res.end("mai server ke andar hu")
+})
+
+server.listen(3000,()=>{
+    console.log("server is running at port 3000")
+
+})
